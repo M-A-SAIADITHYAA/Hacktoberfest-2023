52b821324ad8113a6f4658ec40678dfa6b81dbf4
M-A-SAIADITHYAA <145114909+M-A-SAIADITHYAA@users.noreply.github.com>
2023-10-21 19:12:37 +0530

    Create add numbers.py

---
+++ Basic-Programming/Add_two_Numbers/add numbers.py
@@ -0,0 +1,8 @@
+l1 = eval(input("enter the list(enter in[]and each element separated by a comma)"))
+l2 = eval(input ("enter the list(enter in[]and each element separated by a comma)"))
+res1 =[]
+for i in range(0,len(l1)):
+    res1.append(l1[i]+l2[i])
+print(res1)
+
+    
